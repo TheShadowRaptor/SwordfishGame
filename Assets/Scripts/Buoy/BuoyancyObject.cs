03770b3374245dbb2d4271a0dd47b618b8245801
Tobias Gagnon <90637945+TheShadowRaptor@users.noreply.github.com>
2023-04-08 10:21:54 -0300

    [ADDED] BasicWaterShader

---
+++ Assets/Scripts/Buoy/BuoyancyObject.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class BuoyancyObject : MonoBehaviour
+{
+    public float underWaterDrag = 3;
+    public float underWaterAnglerDrag = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
